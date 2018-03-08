a18ae98f46a845d1853c984b28ac5173f920995a
angel <a.ferrera722@gmail.com>
2018-03-08 17:02:59 -0500

    Initial commit/ added body and eyes

---
+++ sketch.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+
+  function setup() {
+ createCanvas(550,470);
+
+}
+
+function draw() {
+background(255);
+fill(0,70,32);
+ellipse(170,330,280,160);
+fill (255,250,255);
+  ellipse(250, 300, 30, 70);
+
+fill (0,0,0);
+  ellipse(255, 300, 10, 40);
+  
+
+
+
+
+}
