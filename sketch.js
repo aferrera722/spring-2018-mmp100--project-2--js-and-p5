b34d3f2c628e963718578ce114833160da52bf68
angel <a.ferrera722@gmail.com>
2018-03-08 18:26:40 -0500

    added details to fish.

--- sketch.js
+++ sketch.js
@@ -8,7 +8,8 @@
 }
 
 function draw() {
-background(255);
+  colorMode(HSB);
+  background(255, 204, 100);
 fill(0,70,32);
 ellipse(170,330,280,160);
 fill (255,250,255);
@@ -16,9 +17,27 @@ fill (255,250,255);
 
 fill (0,0,0);
   ellipse(255, 300, 10, 40);
-  
+  fill( 0,90,32);
 
+  arc(185, 340, 70, 70, 0, HALF_PI);
+  arc(180, 340, 70, 70, 0, HALF_PI);
+  arc(175, 340, 70, 70, 0, HALF_PI);
+  arc(170, 340, 70, 70, 0, HALF_PI);
+  arc(165, 340, 70, 70, 0, HALF_PI);
+  arc(160, 340, 70, 70, 0, HALF_PI);
 
+  arc(20, 330, 90, 90, 0, HALF_PI);
+  arc( 20, 330, 90, 100, 180, HALF_PI);
+ellipse (180,320, 80, 40);
+fill(70,120,150);
+ 
+ellipse(120,385,10,10);
+ellipse(130,350,10,10);
+ellipse(140,300,10,10);
+ellipse(150,370,10,10);
+ellipse(100, 295, 10, 10);
+ellipse(110, 290, 10, 10);
+ellipse(106, 360, 10, 10);
 
 
 }
